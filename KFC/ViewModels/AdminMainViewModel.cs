3d89d31e270a744c225f44bbc7b95923fc9b8bd1
vladosss5 <kiporovv1@gmail.com>
2023-12-14 15:37:17 +0700

    профиль, смена пароля, отчёт выручки

--- KFC/ViewModels/AdminMainViewModel.cs
+++ KFC/ViewModels/AdminMainViewModel.cs
@@ -1,17 +1,33 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Reactive;
+using System.Windows.Input;
+using Avalonia.Controls;
 using KFC.Models;
+using KFC.Views;
 using ReactiveUI;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace KFC.ViewModels;
 
 public class AdminMainViewModel : ViewModelBase
 {
     private PageViewModelBase _CurrentPage;
-    
+
+    private ObservableCollection<Order> _orders;
+
     public PageViewModelBase CurrentPage
     {
         get { return _CurrentPage; }
         private set { this.RaiseAndSetIfChanged(ref _CurrentPage, value); }
+    }
+
+    public ObservableCollection<Order> Orders
+    {
+        get => _orders;
+        set => this.RaiseAndSetIfChanged(ref _orders, value);
     }
     
     private readonly PageViewModelBase[] Pages = 
@@ -26,10 +42,17 @@ public class AdminMainViewModel : ViewModelBase
     public ICommand OpenEmployeesPage { get; }
     public ICommand OpenProfilePage { get; }
     public ICommand OpenOrdersPage { get; }
+    
+    public ReactiveCommand<Window, Unit> ExitProfile { get; }
+    public ReactiveCommand<Window, Unit> CreateReport { get; }
 
     public AdminMainViewModel()
     {
         _CurrentPage = Pages[0];
+        ExitProfile = ReactiveCommand.Create<Window>(ExitProfileImpl);
+        CreateReport = ReactiveCommand.Create<Window>(CreateReportImpl);
+
+        Orders = new ObservableCollection<Order>(Helper.GetContext().Orders.ToList());
         
         var canOpenMenuDish = this.WhenAnyValue(x => x.CurrentPage.OpenMenuDishesPage);
         OpenMenuDishPage = ReactiveCommand.Create(OpenMenuDishPageImpl, canOpenMenuDish);
@@ -42,6 +65,77 @@ public class AdminMainViewModel : ViewModelBase
 
         var canOpenOrdersPage = this.WhenAnyValue(x => x.CurrentPage.OpenOrdersPage);
         OpenOrdersPage = ReactiveCommand.Create(OpenOrdersPageImpl, canOpenOrdersPage);
+    }
+
+    private void CreateReportImpl(Window obj)
+    {
+        using(ExcelHelper helper = new ExcelHelper())
+        {
+            if (helper.Open(filePath: Path.Combine(Environment.CurrentDirectory, @"C:\Users\Public\Documents\Отчёт о заказах.xlsx")))
+            {
+                int i = 0;
+                var allOrders = Orders.ToList().OrderBy(p => p.DateAndTime).ToList();
+                var application = new Excel.Application();
+                string[] month = new string[12] { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", 
+                                                            "Август", "Сентябрь", "Окрябрь", "Ноябрь", "Декабрь" };
+                
+                application.SheetsInNewWorkbook = month.Length;
+
+                Excel.Workbook workbook = application.Workbooks.Add(Type.Missing);
+                
+                for (int j = 0; j < month.Length; ++j)
+                {
+                    int counter = 0;
+                    int startRowIndex = 1;
+                    
+                    Excel.Worksheet worksheet = application.Worksheets.Item[j + 1];
+                    worksheet.Name = month[j];
+
+                    worksheet.Cells[1][startRowIndex] = "Номер";
+                    worksheet.Cells[2][startRowIndex] = "Дата";
+                    worksheet.Cells[3][startRowIndex] = "Цена";
+
+                    startRowIndex++;
+
+                    while (allOrders.Count > i)
+                    {
+                        if (allOrders[i].DateAndTime.Month == j + 1)
+                        {
+                            worksheet.Cells[1][startRowIndex] = allOrders[i].IdOrder;
+                            worksheet.Cells[2][startRowIndex] = allOrders[i].DateAndTime.ToString("yy-MM-dd");
+                            worksheet.Cells[3][startRowIndex] = allOrders[i].Price;
+                            counter++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                        i++;
+                        startRowIndex++;
+                    }
+                    
+                    Excel.Range sumRange = worksheet.Range[worksheet.Cells[1][startRowIndex],
+                        worksheet.Cells[2][startRowIndex]];
+                    sumRange.Merge();
+                    sumRange.Value = "Итого:";
+
+                    worksheet.Cells[3][startRowIndex].Formula =
+                        $"=SUM(C{startRowIndex - counter}:" + $"C{startRowIndex - 1}";
+
+                    worksheet.Columns.AutoFit();
+                    helper.Save();
+                }
+                application.Visible = true;
+            }
+        }
+    }
+
+    private void ExitProfileImpl(Window obj)
+    {
+        AuthorizationViewModel.AuthUser = null;
+        AuthorizationView av = new AuthorizationView();
+        av.Show();
+        obj.Close();
     }
 
     private void OpenOrdersPageImpl()
--- KFC/ViewModels/ProfilePageViewModel.cs
+++ KFC/ViewModels/ProfilePageViewModel.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Reactive;
+using Avalonia.Controls;
+using KFC.Models;
+using KFC.Views;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
 using ReactiveUI;
 
 namespace KFC.ViewModels;
@@ -6,6 +12,12 @@ namespace KFC.ViewModels;
 public class ProfilePageViewModel : PageViewModelBase
 {
     private bool _OpenProfilePage;
+    
+    public static User _AuthUserNow { get; set; }
+    
+    private string _firstpassword;
+    private string _secondpassword;
+    private string _oldpassword;
     
     public override bool OpenProfilePage
     {
@@ -32,8 +44,64 @@ public class ProfilePageViewModel : PageViewModelBase
         protected set => throw new NotSupportedException();
     }
     
+    public string OldPassword
+    {
+        get => _oldpassword;
+        set => this.RaiseAndSetIfChanged(ref _oldpassword, value);
+    }
+    public string FirstPassword
+    {
+        get => _firstpassword;
+        set => this.RaiseAndSetIfChanged(ref _firstpassword, value);
+    }
+    public string SecondPassword
+    {
+        get => _secondpassword;
+        set => this.RaiseAndSetIfChanged(ref _secondpassword, value);
+    }
+    
+    public ReactiveCommand<Window, Unit> ChangePassword { get; }
+    
     public ProfilePageViewModel()
     {
+        _AuthUserNow = AuthorizationViewModel.AuthUser;;
         OpenProfilePage = false;
+        ChangePassword = ReactiveCommand.Create<Window>(ChangePasswordImpl);
+    }
+    
+    private void ChangePasswordImpl(Window obj)
+    {
+        if (_oldpassword == AuthorizationViewModel.AuthUser.Password)
+        {
+            if (_oldpassword != _firstpassword)
+            {
+                if (_firstpassword == _secondpassword)
+                {
+                    AuthorizationView av = new AuthorizationView();
+                    AuthorizationViewModel.AuthUser.Password = _firstpassword;
+                    Helper.GetContext().Users.Update(AuthorizationViewModel.AuthUser);
+                    Helper.GetContext().SaveChanges();
+                    MessageBoxManager.GetMessageBoxStandard("Успех", "Пароль изменён", ButtonEnum.Ok, Icon.Success).ShowWindowDialogAsync(obj);
+                    av.Show();
+                    obj.Close();
+                }
+                else
+                {
+                    MessageBoxManager.GetMessageBoxStandard("Ошибка","Пароли не совпадают",  ButtonEnum.Ok, Icon.Error).ShowWindowDialogAsync(obj);
+                    return;
+                }
+            }
+            else
+            {
+                MessageBoxManager.GetMessageBoxStandard("Ошибка","Нельзя использовать старый пароль",  ButtonEnum.Ok, Icon.Error).ShowWindowDialogAsync(obj);
+                return;
+            }
+            
+        }
+        else
+        {
+            MessageBoxManager.GetMessageBoxStandard("Ошибка","Неверный текущий пароль",  ButtonEnum.Ok, Icon.Error).ShowWindowDialogAsync(obj);
+            return;
+        }
     }
 }
